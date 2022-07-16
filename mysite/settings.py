f8660044372a22603d94e94674db6702b4699fd0
Kuang Yu Heng <yuheng3107@gmail.com>
2022-07-16 16:54:01 +0800

    Fixed issues with static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'statics/'
+STATIC_URL = 'static/'
+
 
 STATICFILES_DIRS = [
-    BASE_DIR / "statics",
+    BASE_DIR / "static",
+    
 ]
 
 # Default primary key field type
--- translators/views.py
+++ translators/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Novel
+from .forms import NovelForm
 
 from django.views.generic.list import ListView
+from django.views import View
 
 class IndexView(ListView):
     model = Novel
 
 def index(request):
-    return TemplateResponse(request, "translators/index.html")
+    return render(request, "translators/index.html")
+
+class NovelFormView(View):
+
+    template_name = "translators/novel_form.html"
+    success_url = reverse_lazy('translators:index')
+    def get(self, request):
+        form = NovelForm()
+        ctx = {"ctx": form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request):
+        form = NovelForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {"form": form}
+            return render(request, self.template_name, ctx)
+
+        novel = form.save()
+        return redirect(self.success_url)
 
 
-class NovelFormView()
+
 

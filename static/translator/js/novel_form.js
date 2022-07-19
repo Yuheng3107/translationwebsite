941781f9228194ed39888661df48c98cfc8baaac
AppleJem <98686428+AppleJem@users.noreply.github.com>
2022-07-19 21:25:01 +0800

    :Revert "Translator's novel form add JS to display filename"
    This reverts commit 621fac5a98333f9b69470ae94223226cf95169da.

--- static/translator/js/novel_form.js
+++ static/translator/js/novel_form.js
@@ -3,20 +3,11 @@
 window.addEventListener("DOMContentLoaded", (e)=>{
     const picLabel = document.querySelector("label[for=id_picture]");
     const picture = document.querySelector("#id_picture");
-    const rawsLabel = document.querySelector("label[for=id_raws]")
-    const raws = document.querySelector("#id_raws");
     picture.onchange = function () {
         if (picture.files.length){
             picLabel.innerHTML = picture.files[0].name;
         } else {
             picLabel.innerHTML = "no file chosen"
-        }
-    }
-    raws.onchange = function () {
-        if (raws.files.length){
-            rawsLabel.innerHTML = raws.files[0].name;
-        } else {
-            rawsLabel.innerHTML = "no file chosen"
         }
     }
 
